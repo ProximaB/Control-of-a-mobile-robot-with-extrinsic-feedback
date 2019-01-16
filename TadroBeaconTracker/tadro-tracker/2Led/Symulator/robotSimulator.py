d3dab334018851fe9ae4b5e12d8e1052fca70109
Bartek <bartek.smarzeniec@gmail.com>
2019-01-16 23:47:53 +0100

    robotSimulatorEnv

--- TadroBeaconTracker/tadro-tracker/2Led/Symulator/robotSimulator.py
+++ TadroBeaconTracker/tadro-tracker/2Led/Symulator/robotSimulator.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+﻿import cv2 as cv
 import numpy as np
 import sys
 import operator
@@ -29,18 +29,19 @@ ROBOT_THICKNESS = 3
 AREA_POINTS = [(10,10), (-10,-10)] #punkty pola dodawane marginesy wys szer
 AREA_THICKNESS = 4
 
+def add(a, b):
+    '''dodanie dwoch punktow, tuple '''
+    return tuple(map(operator.add, a, b))
+    
 class robotSimulationEnv:
+    def __init__(self, model : RobotModel2Led, tracker = None):
+        self.model = model
+        self.tracker = tracker
 
-    def __init__(model : RobotModel2Led):
-        return super().__init__(*args, **kwargs)
-
-    def add(self, a, b):
-        '''dodanie dwoch punktow, tuple '''
-        return tuple(map(operator.add, a, b))
-    def draw_robot_position(self, robot : Robot2Led, frame):
+    def draw_robot_position(self, frame):
         #sw = statusWindowText(frame)
         #sw.drawData((50,50), 1.23, 10, 1.42, (255,0,0))
-        
+        robot =  self.model.robot
         led1_pos, led2_pos, time, robot_center, heading, diamater, axle_len = robot.unpack()
         #leds
         cv.circle(frame, led1_pos, LED_RADIUS, (0, 255, 0), LED_THICKNES)
@@ -59,26 +60,17 @@ class robotSimulationEnv:
         shape_hw = frame.shape[1::-1]
         cv.rectangle(frame, AREA_POINTS[0], add(shape_hw, AREA_POINTS[1]), 0, AREA_THICKNESS)
 
-    def simulation_keys_KLIO(robot):
+    def simulation_keys_KLIO(self):
         win_frame = np.ones((W_HEIGHT, W_WIDTH, 3), dtype='uint8')
         display_frame = np.ones((D_HEIGHT, D_WIDTH, 3), dtype='uint8') *255 # white plane
 
         #Symulacja Robota
-        # -> wrzucamy model z potrzebnymi parametrami, ktory zwraca pozycje w danym czasie
-        robot = Robot2Led(20, (500, 500), (500, 480), (500, 520), 0, 75, 50, 5)
         #rysowanie_pozycji robota
-        draw_robot_position(robot, display_frame)
-
-        model = RobotModel2Led(robot, 5)
-        model.simulate_robot_process(0,0,1)
-
-        cv.imshow('result', win_frame)
-
         L = 0; R = 0
         while True:
             display_frame = np.ones((D_HEIGHT, D_WIDTH, 3),
                                     dtype='uint8') * 255  # white plane
-            key = cv.waitKey(10)
+            key = cv.waitKey(5)
 
             if key == ord('i'):
                 L += 0.02
@@ -88,18 +80,22 @@ class robotSimulationEnv:
                 L -= 0.02
             elif key == ord('l'):
                 R -= 0.02
+            elif key == ord('q'):
+                break;
+
             print(f'L:{L} R:{R}')
             model.simulate_robot_process(L, R, 1.0)
             robot.calculate_led_pos()
-            #phi = math.atan2(p1[0]-p2[0], p1[1]-p2[1])
-            draw_robot_position(robot, display_frame)
+
+            self.draw_robot_position(display_frame)
+            #wyświetlanie okna prezentującego symulacje i ważne parametry robota
             win_frame = np.ones((W_HEIGHT, W_WIDTH, 3), dtype='uint8') #czyszczenie win_frame
             win_frame[D_MARGIN_VERTICAL[0] : - D_MARGIN_VERTICAL[1], D_MARGIN_HORIZONTAL[0] : - D_MARGIN_HORIZONTAL[1], ] = display_frame
             # wypisywanie statusu
             sw = statusWindowText(win_frame)
             sw.drawData(robot.robot_center, robot.heading, 0, 0)
-            cv.imshow('result', win_frame)  
-            #cv.waitKey(300)
+            #wyswietlenie na oknie
+            cv.imshow('result', win_frame)
         # nakładanie display_frame na win_frame
         win_frame[D_MARGIN_VERTICAL[0] : - D_MARGIN_VERTICAL[1], D_MARGIN_HORIZONTAL[0] : - D_MARGIN_HORIZONTAL[1], ] = display_frame
 
@@ -111,7 +107,9 @@ class robotSimulationEnv:
         cv.waitKey(0)
 
 if __name__ == "__main__":
-    sim = robotSimulator()
+    robot = Robot2Led(20, (500, 500), (500, 480), (500, 520), 0, 75, 50, 5)
+    model = RobotModel2Led(robot, 5)
+    sim = robotSimulationEnv(model, None)
     sim.simulation_keys_KLIO()
     #simulation_test_robot()
     #simulation_keys_KLIO()
